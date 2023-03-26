82b7caedcf3024a78aee30df70d33160ba495856
Makio-eng <vivienne_hskw@yahoo.co.jp>
2023-03-26 14:59:46 +0900

    下層ページヘッダー完了

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-//ヘッダー制御
-window.addEventListener('scroll', headerChange);
+'use strict';
+
+/* ヘッダー
+---------------------------------------------------*/
+const header = document.querySelector('header');
+// 下層ページはアニメーション無しで固定
+if (!header.classList.contains('under-header')) {
+  window.addEventListener('scroll', headerChange);
+}
 function headerChange() {
-  const header = document.querySelector('.header');
   let headerHeight = header.clientHeight;
   if (window.scrollY > headerHeight) {
     header.classList.add('fixed');
@@ -18,7 +24,8 @@ function headerChange() {
   }
 }
 
-// カレンダー（範囲選択）
+/* カレンダー
+---------------------------------------------------*/
 flatpickr('#flatpickr', {
   locale: 'ja',
   minDate: 'today',
@@ -31,7 +38,8 @@ flatpc.addEventListener('change', function () {
   flatpc.value = flatpc.value.replace('から', ' 〜 ');
 });
 
-// セレクトボックスで初期値選択時はグレー表示
+/* セレクトボックスで初期値選択時はグレー表示
+---------------------------------------------------*/
 function changeColor(plan) {
   if (plan.value === '') {
     plan.style.color = 'gray';
@@ -40,7 +48,9 @@ function changeColor(plan) {
   }
 }
 
-// モーダル
+/* モーダル
+---------------------------------------------------*/
+
 const reserveBtn = document.querySelector('.reserve-btn');
 const ovarLay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-icon');
@@ -59,7 +69,8 @@ function toggleModal() {
   body.classList.toggle('no-scroll');
 }
 
-// swiper
+/* swiper
+---------------------------------------------------*/
 const swiper = new Swiper('.swiper', {
   loop: true,
   effect: 'fade', // フェード切り替え
@@ -70,3 +81,37 @@ const swiper = new Swiper('.swiper', {
   },
   speed: 2000, // 2秒かけてフェード
 });
+
+/* タブ
+---------------------------------------------------*/
+const tabMenus = document.querySelectorAll('.tab__menu-item');
+// イベント付加
+tabMenus.forEach((tabMenu) => {
+  tabMenu.addEventListener('click', tabSwitch);
+});
+
+function tabSwitch(e) {
+  // クリックされた要素のデータ属性を取得
+  const tabTargetData = e.currentTarget.dataset.tab;
+  // クリックされた要素の親要素と、その子要素を取得
+  const tabList = e.currentTarget.closest('.tab__menu');
+  const tabItems = tabList.querySelectorAll('.tab__menu-item');
+  // クリックされた要素の親要素の兄弟要素の子要素を取得
+  const tabPanelItems =
+    tabList.nextElementSibling.querySelectorAll('.tab__panel-box');
+
+  // クリックされたtabの同階層のmenuとpanelのクラスを削除
+  tabItems.forEach((tabItem) => {
+    tabItem.classList.remove('is-active');
+  });
+  // クリックされたmenu要素にis-activeクラスを付加
+  e.currentTarget.classList.add('is-active');
+
+  // クリックしたmenuのデータ属性と等しい値を持つパネルにis-showクラスを付加
+  tabPanelItems.forEach((tabPanelItem) => {
+    tabPanelItem.classList.remove('is-show');
+    if (tabPanelItem.dataset.panel === tabTargetData) {
+      tabPanelItem.classList.add('is-show');
+    }
+  });
+}
